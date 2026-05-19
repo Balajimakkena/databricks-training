3a81bd16b7327e5135f40562982930f8438ab509
Balajimakkena <pbalaji95871@gmail.com>
2026-05-19 13:17:59 +0530

    initial commit

--- databricks-training/Week02/Day 03/README.md
+++ databricks-training/Week02/Day 03/README.md
@@ -1,1 +1,54 @@
 # --Practice problems on SQL
+# Customer Account Status Evaluation
+
+## Concepts Used
+
+### Numeric Functions
+- `ABS()`
+- `SIGN()`
+
+### Date Functions
+- `DATEDIFF()`
+
+### String Functions
+- `CONCAT()`
+- `UPPER()`
+- `LOWER()`
+
+### Conditional Functions
+- `CASE`
+ 
+
+# SQL Functions Covered
+
+## String Functions
+- `UPPER()`
+- `LOWER()`
+- `CONCAT()`
+- `SUBSTRING()`
+- `SUBSTRING_INDEX()`
+
+---
+
+## Numeric Functions
+- `ROUND()`
+- `TRUNCATE()`
+- `ABS()`
+- `POWER()`
+- `SIGN()`
+
+---
+
+## Date Functions
+- `DATEDIFF()`
+- `TIMESTAMPDIFF()`
+- `MONTHNAME()`
+- `DAYNAME()`
+- `YEAR()`
+- `CURDATE()`
+
+---
+
+## Conditional Functions
+- `CASE`
+- `IFNULL()`
--- databricks-training/Week02/Day 03/StrDataNum.sql
+++ databricks-training/Week02/Day 03/StrDataNum.sql
@@ -320,3 +320,551 @@ Select user_id,
             else 'Active'
          END as subscription_status
 from subscriptions;
+
+
+--QUESTION 5: Loan EMI Risk Categorization
+
+--Table Structure
+
+CREATE TABLE loan_details (
+
+loan_id INT,
+
+customer_name VARCHAR(50),
+
+loan_amount DECIMAL(12,2),
+
+interest_rate DECIMAL(5,2),
+
+loan_start DATE
+
+);
+
+--Insert Data
+
+INSERT INTO loan_details VALUES
+
+(201,'suresh',500000.75,8.5,'2022-01-10'),
+
+(202,'mahesh',750000.40,9.2,'2021-05-20'),
+
+(203,'anita',300000.90,7.8,'2023-07-01');
+
+--Questions
+
+--Compute:
+
+--1· Monthly interest using power function
+Select    
+    loan_id,
+    customer_name,
+    loan_amount,
+    interest_rate,
+    POWER((1 + interest_rate / 100), 1.0/12) - 1 AS monthly_interest,
+FROM loan_details;
+
+--2· Years since loan start
+Select 
+    loan_id,
+    customer_name,
+    loan_amount,
+    interest_rate,
+    TIMESTAMPDIFF(YEAR, loan_start, CURDATE()) AS years_since_loan_start,
+From loan_details;
+
+--3· Round EMI
+Select 
+    loan_id,
+    customer_name,
+    loan_amount,
+    interest_rate,
+    Round(
+        (
+            loan_amount * 
+            ((interest_rate / 100) / 12) * 
+            POWER(
+                1 + ((interest_rate / 100) / 12),
+                TIMESTAMPDIFF(MONTH, loan_start, CURDATE())
+            )
+        ) /
+        (
+            POWER(
+                1 + ((interest_rate / 100) / 12),
+                TIMESTAMPDIFF(MONTH, loan_start, CURDATE())
+            ) - 1
+        ),
+        2) AS EMI
+from loan_details;
+
+--4· Uppercase customer name
+Select 
+    loan_id,
+    UPPER(customer_name) AS customer_name,
+    loan_amount,
+    interest_rate,
+from loan_details;
+
+--5
+/*
+· CASE:
+
+o High Risk if interest > 9
+
+o Medium Risk
+
+o Low Risk
+*/
+Select 
+    loan_id,
+    customer_name,
+    loan_amount,
+    interest_rate,
+        case 
+        WHEN interest_rate > 9 THEN 'High Risk'
+        WHEN interest_rate between 8 and 9 THEN 'Medium Risk'
+        ELSE 'Low Risk'
+    End as  risk_category
+
+From loan_details;
+
+
+--QUESTION 6: Employee Attendance Evaluation
+
+--Table Structure
+
+CREATE TABLE attendance (
+
+emp_id INT,
+
+emp_name VARCHAR(50),
+
+total_days INT,
+
+present_days INT,
+
+record_date DATE
+
+);
+
+--Insert Data
+
+INSERT INTO attendance VALUES
+
+(1,'karthik',30,28,'2025-01-31'),
+
+(2,'veena',30,22,'2025-01-31'),
+
+(3,'ravi',30,18,'2025-01-31');
+
+--Questions
+
+
+--Calculate:
+
+--1· Attendance percentage (rounded)
+Select 
+    emp_id,
+    total_days,
+    present_days,
+    ROUND((present_days * 100.0) / total_days, 2) AS attendance_percentage,
+from attendance;
+
+--2· Month name
+Select 
+    emp_id,
+    MONTHNAME(record_date) AS month_name,
+from attendance;
+
+
+
+--3· Difference between total and present days
+Select 
+    emp_id,
+    total_days,
+    present_days,
+    (total_days - present_days) AS absent_days,
+from attendance;
+
+--4· Lowercase employee name
+Select 
+    emp_id,
+    LOWER(emp_name) AS employee_name,
+from attendance;
+--5
+/*· CASE:
+
+o Excellent ≥90%
+
+o Average 75–89%
+
+o Poor otherwise
+*/
+Select 
+    emp_id,
+    CASE
+        WHEN (present_days * 100.0) / total_days >= 90 
+            THEN 'Excellent'
+            
+        WHEN (present_days * 100.0) / total_days BETWEEN 75 AND 89
+            THEN 'Average'
+            
+        ELSE 'Poor'
+    END AS attendance_status
+from attendance;
+
+
+--QUESTION 7: Product Discount Validation
+
+--Table Structure
+
+CREATE TABLE product_sales (
+
+product_id INT,
+
+product_name VARCHAR(50),
+
+mrp DECIMAL(10,2),
+
+selling_price DECIMAL(10,2),
+
+sale_date DATE
+
+);
+
+--Insert Data
+
+INSERT INTO product_sales VALUES
+
+(1,'Laptop',75000.75,68000.50,'2025-01-10'),
+
+(2,'Mobile',35000.40,33000.25,'2025-01-12'),
+
+(3,'Tablet',25000.90,26000.75,'2025-01-15');
+
+--Questions
+
+--Derive:
+
+--1· Discount amount (absolute)
+Select
+    product_id,
+    mrp,
+    selling_price,
+    ABS(mrp - selling_price) AS discount_amount
+from product_sales;
+
+    
+--2· Discount percentage
+Select
+    product_id,
+    mrp,
+    selling_price,
+    round(((mrp - selling_price) / mrp) * 100, 2) as discount_percentage,
+from product_sales;
+
+
+--3· Day name of sale
+Select
+        product_id,
+        dayname(sale_date) as sale_day,
+from product_sales;
+
+--4· Proper case product name
+Select
+    product_id,
+    concat(
+        UPPER(left(product_name,1)),
+        LOWER(substring(product_name,2))
+    ) as product_name,
+from product_sales;
+--5.
+/*
+· CASE:
+
+o Valid Discount
+
+o Overpriced
+
+o No Discount
+*/
+Select 
+    product_id,
+    CASE
+        WHEN selling_price < mrp THEN 'Valid Discount'
+        WHEN selling_price > mrp THEN 'Overpriced'
+        ELSE 'No Discount'
+    end as  discount_status
+from product_sales;
+
+
+--QUESTION 8: Insurance Policy Aging
+
+--Table Structure
+
+CREATE TABLE insurance_policies (
+
+policy_id INT,
+
+holder_name VARCHAR(50),
+
+premium_amount DECIMAL(10,2),
+
+policy_start DATE,
+
+policy_end DATE
+
+);
+
+--Insert Data
+
+INSERT INTO insurance_policies VALUES
+
+(301,'arjun',12000.50,'2023-01-01','2026-01-01'),
+
+(302,'megha',8500.75,'2022-06-15','2025-06-15'),
+
+(303,'vinod',15000.90,'2021-03-01','2024-03-01');
+
+--Questions
+
+--Show:
+
+--1· Policy duration in years
+Select 
+        policy_id,
+        policy_start,
+        policy_end,
+        timestampdiff(year, policy_start, policy_end) as policy_duration_years
+from insurance_policies;
+
+--2· Remaining days
+Select 
+        policy_id,
+        policy_start,
+        policy_end,
+        datediff(policy_end, curdate()) as remaining_days
+from insurance_policies;
+
+--3· Rounded premium
+Select 
+        policy_id,
+        premium_amount,
+        policy_start,
+        policy_end,
+        Round(premium_amount, 0) AS rounded_premium
+from insurance_policies;
+
+--4· Uppercase holder name
+Select
+    policy_id,
+    upper(holder_name) as holder_name
+from insurance_policies;
+
+
+--5
+/*
+· CASE:
+
+o Long Term
+
+o Mid Term
+
+o Expired
+*/
+Select
+  policy_id,
+  CASE
+        WHEN policy_end < CURDATE() 
+            THEN 'Expired'
+            
+        WHEN timestampdiff(YEAR, policy_start, policy_end) >= 3 
+            THEN 'Long Term'
+            
+        ELSE 'Mid Term'
+    end as  policy_status
+from insurance_policies;
+
+
+--QUESTION 9: Salary Increment Simulation
+
+--Table Structure
+
+CREATE TABLE salary_revision (
+
+emp_id INT,
+
+emp_name VARCHAR(50),
+
+current_salary DECIMAL(10,2),
+
+rating INT,
+
+last_hike DATE
+
+);
+
+--Insert Data
+
+INSERT INTO salary_revision VALUES
+
+(1,'karthik',75000.75,5,'2023-01-01'),
+
+(2,'veena',65000.40,4,'2024-01-01'),
+
+(3,'ravi',85000.90,3,'2022-01-01');
+
+--Question
+
+--Calculate:
+
+--1· Years since last hike
+Select
+        emp_id,
+        current_salary,
+        rating,
+        timestampdiff(year, last_hike, curdate()) as years_since_last_hike
+from salary_revision;
+
+
+--2· Increment using rating logic
+Select
+        emp_id,
+        CASE
+                WHEN rating = 5 THEN current_salary * 0.20
+                WHEN rating = 4 THEN current_salary * 0.10
+                WHEN rating = 3 THEN current_salary * 0.05
+                ELSE 0
+                end as increment_amount
+from salary_revision;
+
+
+--3· New salary (rounded)
+Select 
+        emp_id,
+        round(
+                current_salary +
+                CASE
+                WHEN rating = 5 THEN current_salary * 0.20
+                WHEN rating = 4 THEN current_salary * 0.10
+                WHEN rating = 3 THEN current_salary * 0.05
+                ELSE 0
+                end,2) AS new_salary
+from salary_revision;
+
+--4· Lowercase name
+Select 
+    emp_id,
+    lower(emp_name) as employee_name
+from salary_revision;
+
+--5.
+/*
+· CASE:
+
+o High Increment
+
+o Moderate
+
+o No Increment
+*/
+Select
+        emp_id,
+        CASE
+                WHEN rating = 5 THEN 'High Increment'
+                WHEN rating = 4 THEN 'Moderate'
+                ELSE 'No Increment'
+                end as increment_status       
+from  salary_revision;
+
+
+--QUESTION 10: Customer Account Status Evaluation
+
+--Table Structure
+
+CREATE TABLE bank_accounts (
+
+account_id INT,
+
+customer_name VARCHAR(50),
+
+balance DECIMAL(12,2),
+
+last_transaction DATE,
+
+branch VARCHAR(30)
+
+);
+
+--Insert Data
+
+INSERT INTO bank_accounts VALUES
+
+(501,'ramesh',125000.75,'2024-12-20','hyderabad'),
+
+(502,'sita',8500.40,'2023-06-15','delhi'),
+
+(503,'manoj',-2500.90,'2025-01-05','mumbai');
+
+--Questions
+
+--Determine:
+
+--1· Absolute balance
+Select 
+        account_id,
+        customer_name,
+        abs(balance) as absolute_balance
+from bank_accounts;
+
+
+--2· Days since last transaction
+Select 
+        account_id,
+        customer_name,
+        datediff(curdate(), last_transaction) as days_since_last_transaction
+from bank_accounts;
+
+--3· Proper case branch name
+Select 
+        account_id,
+        customer_name,
+        concat(
+        upper(left(branch,1)),
+        lower(substring(branch,2))
+                ) AS branch_name
+from bank_accounts;
+
+
+--4· Sign of balance
+Select 
+        account_id,
+        customer_name,
+        Sign(balance) as balance_sign
+from bank_accounts;
+
+--5.
+/*
+· CASE:
+
+o Active
+
+o Dormant
+
+o Overdrawn
+*/
+Select 
+        account_id,
+        customer_name,
+        CASE
+                WHEN balance < 0 
+                THEN 'Overdrawn'
+                
+                WHEN DATEDIFF(CURDATE(), last_transaction) > 365 
+                THEN 'Dormant'
+                
+                ELSE 'Active'
+        end as  account_status
+from bank_accounts;
+
+
+
