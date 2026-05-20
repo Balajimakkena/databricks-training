d5883d8d0466f3b760b708ca14feb0c5eb5fcc78
Balajimakkena <pbalaji95871@gmail.com>
2026-05-20 23:18:39 +0530

    initial commit

--- Week03/day01/session-1 (1).py
+++
@@ -1,8 +0,0 @@
-# Databricks notebook source
-df = spark.read.format("csv").load("/Volumes/workspace/default/myfiles/empData.csv")
-display(df)
-
-# COMMAND ----------
-
-df = spark.read.format("csv").load("/Volumes/workspace/default/myfiles/Big Sales.csv")
-display(df)
